7e6aa28f7caea101eef0f97bfe634defe79b27a9
danrahn <danrahn@uw.edu>
2014-04-25 21:37:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,189 +1,3 @@
-Skip to content
- 
-This repository
-Explore
-Gist
-Blog
-Help
-danrahn danrahn
- 
-!
-
-You don't have any verified emails. We recommend verifying at least one email.
-Email verification helps our support team help you in case you have any email issues or lose your password.
-5  Unwatch
-Star 0 Fork 4PUBLICaarongu/FbSwag
- branch: master  FbSwag / index.php 
-danrahn danrahn 44 minutes ago Create index.php
-1 contributor
- file  168 lines (146 sloc)  5.031 kb  Open EditRawBlameHistory Delete
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
-49
-50
-51
-52
-53
-54
-55
-56
-57
-58
-59
-60
-61
-62
-63
-64
-65
-66
-67
-68
-69
-70
-71
-72
-73
-74
-75
-76
-77
-78
-79
-80
-81
-82
-83
-84
-85
-86
-87
-88
-89
-90
-91
-92
-93
-94
-95
-96
-97
-98
-99
-100
-101
-102
-103
-104
-105
-106
-107
-108
-109
-110
-111
-112
-113
-114
-115
-116
-117
-118
-119
-120
-121
-122
-123
-124
-125
-126
-127
-128
-129
-130
-131
-132
-133
-134
-135
-136
-137
-138
-139
-140
-141
-142
-143
-144
-145
-146
-147
-148
-149
-150
-151
-152
-153
-154
-155
-156
-157
-158
-159
-160
-161
-162
-163
-164
-165
-166
-167
 <html>
 <head>
 	<title>Test App</title>
@@ -264,6 +78,7 @@ danrahn danrahn 44 minutes ago Create index.php
 FB.Event.subscribe('auth.authResponseChange', function(response) {
     if (response.status === 'connected') {
       console.log('Logged in');
+	  printPictures();
 	  printNames();
 	  testAPI();
     } else {
@@ -271,7 +86,27 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
     }
   });
   
+  function printPictures() {
+	FB.api('me/friends', function(response) {
+		var names = new Array();
+		var ids = new Array();
+		for (var i = 0; i < response.data.length; i++) {
+			names[i] = response.data[i]['name'];
+			ids[i] = response.data[i]['id'];
+		}
+		FB.api(ids[i] + '/photos', function(response) {
+			var pictures = new Array();
+			for (var i = 0; i < 5; i++) {
+				pictures[i] = response.data[i]['source'];
+				console.log(response.data[i]['source']);
+				document.getElementById('images').innerHTML += ('<img src="' + response.data[i]['source'] + '" alt="image" />');
+			}
+		});
+	});
+  };
+  
   function printNames() {
+  var ids = new Array();
   $(function() {
 	FB.api('/me/friends', function(response) {
 		/*
@@ -282,8 +117,10 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
 		document.getElementById("text").innerHTML = (text + '</select><input type="submit" value="Go" /></form>');
 		*/
 		var availableTags = new Array(); 
+		var ids = new Array();
 		for (var i = 0; i < response.data.length; i++) {
 			availableTags[i] = response.data[i]['name'];
+			ids[i] = response.data[i]['id'];
 			/*
 			document.getElementById("text").innerHTML += (response.data[i]['name'] + '<img src="https://graph.facebook.com/' + response.data[i]['id'] + '/picture" alt="pic" /><br />');
 			*/
@@ -294,7 +131,7 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
 		}
 	});
   });
-
+	
   }
 };
 
@@ -324,9 +161,9 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
   <!-- Begin page content -->
   <div class="container">
     <div class="page-header">
-      <h1>Sticky footer</h1>
+      <h1>Name</h1>
     </div>
-    <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS.</p>
+    <p class="lead" id="images"></p>
     <p id="text"></p>
     <div class="ui-widget">
     <form action="//webster.cs.washington.edu/params.php">
@@ -344,11 +181,9 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
 
 <div id="footer">
   <div class="container">
-    <p class="muted credit"><fb:login-button show-faces="true" width="300" max-rows="1"></fb:login-button></p>
   </div>
 </div>
     	
 </div>
 </body>
 </html>
-Status API Training Shop Blog About © 2014 GitHub, Inc. Terms Privacy Security Contact 
