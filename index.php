396ac1bab78a815b979b0de25c48ac248fa4c898
Eden Ghirmai <ghirme@uw.edu>
2014-04-26 10:49:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<link href="index.css" rel="stylesheet" type="text/css" />
 	<script src="toggle.js" type="text/javascript"></script>
     
-    <link href="css/style.css.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
