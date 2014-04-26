c01aeda5222feff9becfa0ec5e7d61714ae4ea62
Eden Ghirmai <ghirme@uw.edu>
2014-04-26 13:45:35 -0700

    Update index.php

--- index.php
+++ index.php
@@ -243,9 +243,9 @@ FB.Event.subscribe('auth.authResponseChange', function(response) {
          for (var i = 1; i <= 4; i++) {
            //document.getElementById('demo3').innerHTML+=(commentnames[i][0] + ' count ' + commentnames[i][1] + '<br />');
            document.getElementById("comments_graph_name_" + i).innerHTML = commentnames[i - 1][0];
-           //console.log(comment[i - 1][1]);
-           //console.log(comment[0][1]);
-           document.getElementById("comments_graph_" + i).style.width = parseInt(100.0 * commentnames[i - 1][1] / commentnames[0][1]) + " %";
+           console.log(commentnames[i - 1][1]);
+           console.log(commentnames[0][1]);
+           document.getElementById("comments_graph_" + i).style.width = parseInt(100.0 * commentnames[i - 1][1] / commentnames[0][1], 10) + " %";
            
            
            //document.getElementById("comments_graph_" + i).style.width = "100%"; 
