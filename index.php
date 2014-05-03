42093ec6f4c31224a5e4dcccee6e7325d1a5bf85
danrahn <danrahn@uw.edu>
2014-05-03 14:49:39 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+
+$useragent=$_SERVER['HTTP_USER_AGENT'];
+if(preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4)))
+header('Location: http://fathomless-spire-1079.herokuapp.com/mobile.php');
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -125,7 +133,10 @@ window.fbAsyncInit = function() {
 			document.getElementById("login").style.display="none";
 			document.getElementById("loginAfter").style.display="block";
 			// drawUI();
-			main();
+			if (!loggedIn) {
+				loggedIn = false;
+				main();
+			}
 			testAPI();
 			loggedIn = true;
 		} else if (response.status === 'not_authorized') {
@@ -252,8 +263,11 @@ window.fbAsyncInit = function() {
 			var pictures = new Array();
 			var totalLikes = new Array();
 			var totalComments = new Array();
-			var call = id + "/albums?limit=1&fields=name,photos.limit(100).since(" + since + ").fields(source,likes.limit(1000),comments.limit(1000)),count";
-			getPictures(id, call, 0, 0, since, pictures, totalLikes, totalComments, name);
+			if (since == 0) {
+				var call = id + "/albums?limit=1&fields=name,photos.limit(100).since(" + since + ").fields(source,likes.limit(1000),comments.limit(1000)),count";
+				getPictures(id, call, 0, 0, since, pictures, totalLikes, totalComments, name);
+			} else
+				getPicturesOld(id, 0, since, pictures, totalLikes, totalComments, name, 0, 0);
 		});
 	}
 	
@@ -319,7 +333,7 @@ window.fbAsyncInit = function() {
 		else if (index > 5000 && index <= 10000)
 			document.getElementById('processing').innerHTML="<h1>Processing...</h1><h2>(" + (index) + " Pictures Processed)</h2><h3>Really? Over 5000?</h3>";
 		else
-			document.getElementById('processing').innerHTML="<h1>Processing...</h1><h2>(" + (index) + " Pictures Processed)</h2><h3>Over 10,000???</h3>"
+			document.getElementById('processing').innerHTML="<h1>Processing...</h1><h2>(" + (index) + " Pictures Processed)</h2><h3>Over 10,000???</h3>";
 		FB.api(call, function(response) {
 			var album;
 			var dat = response.data[0];
@@ -329,7 +343,13 @@ window.fbAsyncInit = function() {
 				if ((dat['name']) != null) {
 					console.log(dat.name + ": " + dat.count);
 					if (dat.count > 0)
-						photos = dat.photos.data;
+						if (dat.photos != null)
+							photos = dat.photos.data;
+						else {
+							var nextCall = id + "/albums?offset=" + (offset + 1) + "&limit=1&fields=name,photos.limit(100).since(" + since + ").fields(source,likes.limit(1000),comments.limit(1000)),count";
+						getPictures(id, nextCall, index, offset + 1, since, pictures, totalLikes, totalComments, name);
+						return;
+						}
 					else {
 						var nextCall = id + "/albums?offset=" + (offset + 1) + "&limit=1&fields=name,photos.limit(100).since(" + since + ").fields(source,likes.limit(1000),comments.limit(1000)),count";
 						getPictures(id, nextCall, index, offset + 1, since, pictures, totalLikes, totalComments, name);
@@ -380,7 +400,6 @@ window.fbAsyncInit = function() {
 							}
 						}
 					}
-					
 					index += photos.length;
 				}
 				console.log(index + " total pictures so far");
@@ -411,6 +430,69 @@ window.fbAsyncInit = function() {
 				console.log("Final picture count: " + index);
 				processPictureData(id, since, pictures, totalLikes, totalComments, name);
 			}
+		});
+	}
+	
+	function updateTicker(index) {
+		console.log(index);
+		if (index <=5000)
+			document.getElementById('processing').innerHTML="<h1>Processing...</h1><h2>(" + (index) + " Pictures Processed)</h2>";
+		else if (index > 5000 && index <= 10000)
+			document.getElementById('processing').innerHTML="<h1>Processing...</h1><h2>(" + (index) + " Pictures Processed)</h2><h3>Really? Over 5000?</h3>";
+		else
+			document.getElementById('processing').innerHTML="<h1>Processing...</h1><h2>(" + (index) + " Pictures Processed)</h2><h3>Over 10,000???</h3>";
+	}
+	
+	function getPicturesOld(id, offset, since, pictures, totalLikes, totalComments, name, index, lastIndex) {
+		var lastIndex;
+		if (index == 0)
+				document.getElementById('processing').innerHTML="<h1>Processing...</h1><h2>(Waiting for response from server)</h2>";
+		FB.api((id + "/photos/uploaded?limit=2500&since=" + since + "&offset=" + offset + "&fields=likes.limit(1000),comments.limit(1000),source"), function(response) {
+			for (var i = 0; i < response.data.length; i++) {
+				updateTicker(index);
+				index++;
+				var likes = 0;
+				var dat = response.data[i];
+				if (dat.likes != null)
+					likes = dat.likes.data.length; //EDITED, CHECK
+				pictures.push(new Array(dat.source, likes));
+				for (var j = 0; j < likes; j++) {
+					var liker = dat.likes.data[j].name;
+					var found = false;
+					for (var k = 0; k < totalLikes.length; k++) {
+						if (totalLikes[k][0] == liker) {
+							totalLikes[k][1]++;
+							found = true;
+							break;
+						}
+					}
+					if (!found)
+						totalLikes.push(new Array(liker, 1));
+				}
+				var comments = 0;
+				if (dat.comments != null) {
+					comments = (dat.comments.data).length;
+				}
+				for (var j = 0; j < comments; j++) {
+					var commentor = dat.comments.data[j].from.name;
+					if (commentor != name) {
+						var found = false;
+						for (var k = 0; k < totalComments.length; k++) {
+							if (totalComments[k][0] == commentor) {
+								totalComments[k][1]++;
+								found = true;
+								break;
+							}
+						}
+						if (!found)
+							totalComments.push(new Array(commentor, 1));
+					}
+				}
+			}
+			if (response.paging != null && response.paging['next'] != null)
+				getPicturesOld(id, offset + 2500, since, pictures, totalLikes, totalComments, name, index, lastIndex);
+			else
+				processPictureData(id, since, pictures, totalLikes, totalComments, name);
 		});
 	}
 	
@@ -1026,7 +1108,7 @@ function testAPI() {
       </div>
         
         <div id="noParam" style="width: 100%;text-align:center;font-family:'Myriad Pro','Lucidia Grande', Helvetica, sans-serif;font-size:24pt;margin-top: 10%">
-        	<h1>Welcome to QuickView! Enter a name in the search box to the right to get started!</h1>
+        	<h1>Welcome to QuickView! Enter a name in the search box to the right to get started.</h1>
         </div>
         
         <div id="processing">
